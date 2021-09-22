3d8a5cff743a3fc9d43e3ccdab7e5804925a0bb8
aliabadi <aliyeh_aliabadi@yahoo.com>
2021-09-22 12:26:03 +0330

    Add Product Domian Model

---
+++ CleanArch/CleanArch.Domain/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Cost { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
