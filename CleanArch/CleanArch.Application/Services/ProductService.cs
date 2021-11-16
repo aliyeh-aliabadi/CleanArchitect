4879a94930438748aaa75633114d7d88b35c9d96
aliyeh_aliabadi <aliyeh_aliabadi@outlook.com>
2021-11-16 13:36:18 +0330

    Commit implementation of service and Repository Products

---
+++ CleanArch/CleanArch.Application/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        ProductViewModel IProductService.GetProducts()
+        {
+            return new ProductViewModel()
+            {
+                Products = _productRepository.GetProducts()
+            };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ProjectDbContext _ctx;
+
+        public ProductRepository(ProjectDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            return _ctx.Products;
+        }
+    }
+}
