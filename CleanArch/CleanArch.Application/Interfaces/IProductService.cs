33ecfebb632af878815e84adaf627b890ebf4d32
aliyeh_aliabadi <aliyeh_aliabadi@outlook.com>
2021-11-16 11:54:50 +0330

    Commit Service and Repository Interfaces, ProductViewModel

---
+++ CleanArch/CleanArch.Application/Interfaces/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using CleanArch.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Interfaces
+{
+    //Our IProductService needs to talk to or domain or backend in order to get our core model or core entity
+    // and then map it back to our course view model and then presend it to the MVC project
+    public interface IProductService
+    {
+        IEnumerable<ProductViewModel> GetProducts();  
+
+    }
+}
---
+++ CleanArch/CleanArch.Application/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Product> Products {  get; set;}
+    }
+}
