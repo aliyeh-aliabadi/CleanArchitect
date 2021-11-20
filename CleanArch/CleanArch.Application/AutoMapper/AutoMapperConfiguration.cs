0d27ee46f3f0d8c4c41f25b7d190c6dd2cefb1e5
aliyeh_aliabadi <aliyeh_aliabadi@outlook.com>
2021-11-20 13:51:45 +0330

    Add AutoMapperConfiguration class

---
+++ CleanArch/CleanArch.Application/AutoMapper/AutoMapperConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.AutoMapper
+{
+    public class AutoMapperConfiguration
+    {
+        //static method to register mapping profiles
+        //MapperConfiguration is a part of AutoMapper library
+        public static MapperConfiguration RegisterMappings()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ViewModelToDomainProfile());
+                cfg.AddProfile(new DomainToViewModelProfile());
+            });
+        }
+    }
+}
