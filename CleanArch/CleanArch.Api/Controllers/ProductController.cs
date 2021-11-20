055a7a079b3ad88ad6b95ee8befd7f472b6ada25
aliyeh_aliabadi <aliyeh_aliabadi@outlook.com>
2021-11-20 10:59:53 +0330

    Add Api Project and Rename Controller

---
+++ CleanArch/CleanArch.Api/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
